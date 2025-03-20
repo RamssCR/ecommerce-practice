b00db5c12023e4803caae5d1c9c2d843f933256c
RamssCR <ramsesgpm@gmail.com>
2025-03-19 23:26:39 -0500

    feat: configured routing elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/header/Header'
+
 export default function App() {
   return (
-    <h1>Hello World!</h1>
+    <main className="w-full flex flex-col items-center">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>} />
+        </Routes>
+        {/* Footer goes here */}
+      </BrowserRouter>
+    </main>
   )
 }
