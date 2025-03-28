8b54bec6f40b079ee11fec383b17382ced5d0054
RamssCR <ramsesgpm@gmail.com>
2025-03-27 20:58:25 -0500

    feat: added categories and sales card components

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,19 @@
-import Banners from '@components/banners/Banners'
+import MainBanner from '@components/banners/MainBanner'
+import Categories from '@components/categories/Categories'
+import SaleCard from '@components/sale-card/SaleCard'
 import Container from '@components/ui/containers/Container'
 
 export default function Home() {
     return (
         <section className="w-full px-4 py-6 bg-neutral-background md:px-8">
             <Container className="mx-auto flex flex-col gap-5">
-                <Banners />
+                <MainBanner />
+                <Categories />
+                <section className="mx-auto w-fit grid grid-cols-1 gap-y-6 md:grid-cols-2 md:gap-x-6 xl:grid-cols-3">
+                    <SaleCard theme="pink" />
+                    <SaleCard theme="purple" />
+                    <SaleCard theme="blue" />
+                </section>
             </Container>
         </section>
     )
