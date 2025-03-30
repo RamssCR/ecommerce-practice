b7865e200678a0ed77004bd6dbccc51b275b5457
RamssCR <ramsesgpm@gmail.com>
2025-03-30 11:51:51 -0500

    styles: modified UI components

--- src/components/ui/containers/Container.tsx
+++ src/components/ui/containers/Container.tsx
@@ -14,7 +14,7 @@ export default function Container({ children, className, ...props }: HTMLAttribu
     return (
         <div
             className={classMerger(
-                'w-full max-w-[25.5em] sm:max-w-[27em] md:max-w-[45em] lg:max-w-[70em] xl:max-w-[77.5em] 2xl:max-w-[90em]',
+                'w-full max-w-[25.5em] sm:max-w-[27em] md:max-w-[45em] lg:max-w-[70em] xl:max-w-[77em] 2xl:max-w-[90em]',
                 className
             )}
             {...props}
