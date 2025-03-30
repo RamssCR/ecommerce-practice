e0507a9e74a4a52164189483e58f0d23586443ea
RamssCR <ramsesgpm@gmail.com>
2025-03-30 11:51:22 -0500

    feat: created service card and mapped items

---
+++ src/components/service-item/ServiceItem.tsx
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import type { FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
+import { useThemeProvider } from '@hooks/useThemeProvider'
+import Title from '@components/ui/texts/Title'
+import Text from '@components/ui/texts/Text'
+
+interface ServiceItemProps extends FontAwesomeIconProps {
+    perk: string
+    description: string
+}
+
+export default function ServiceItem({ icon, perk, description }: ServiceItemProps) {
+    const { theme } = useThemeProvider()
+
+    return (
+        <article className="w-full flex flex-col items-center gap-6 px-5 text-center">
+            <FontAwesomeIcon
+                icon={icon}
+                title={perk}
+                className={`
+                    text-4xl p-5 rounded-full border-2
+                    ${theme === 'dark' ? 'text-neutral-text-highlight border-neutral-text-highlight' : 'text-primary border-primary'}
+                `}
+            />
+            <Title as="h3" className="text-neutral-text-highlight text-2xl font-semibold">{perk}</Title>
+            <Text className="-mt-3">{description}</Text>
+        </article>
+    )
+}
---
+++ src/utils/serviceMap.ts
@@ -0,0 +1,31 @@
+import type { FontAwesomeIconProps } from "@fortawesome/react-fontawesome"
+import { faEarth, faWallet, faShield, faHeadset } from '@fortawesome/free-solid-svg-icons'
+
+type ServicesMap = {
+    icon: FontAwesomeIconProps["icon"]
+    perk: string
+    description: string
+}
+
+export const services: ServicesMap[] = [
+    {
+        icon: faEarth,
+        perk: "Worldwide Shipping",
+        description: "We ship to over 200 countries, ensuring that your products reach you no matter where you are.",
+    },
+    {
+        icon: faWallet,
+        perk: "100% Secure Payment",
+        description: "Your payment information is encrypted and secure, giving you peace of mind while shopping.",
+    },
+    {
+        icon: faShield,
+        perk: "Money Back Guarantee",
+        description: "If you're not satisfied with your purchase, we offer a hassle-free money-back guarantee.",
+    },
+    {
+        icon: faHeadset,
+        perk: "24/7 Customer Support",
+        description: "Our dedicated support team is available 24/7 to assist you with any inquiries or issues.",
+    },
+]
