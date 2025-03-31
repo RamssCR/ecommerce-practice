3cf9eb08f0842dad0c7af74cc88503a3fd4617b4
RamssCR <ramsesgpm@gmail.com>
2025-03-30 21:17:27 -0500

    feat: created and implemented newsletter section

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,6 @@
 import Brand from './Brand'
 import Legal from './Legal'
+import Newsletter from './Newsletter'
 import Container from '@components/ui/containers/Container'
 import Text from '@components/ui/texts/Text'
 
@@ -18,6 +19,7 @@ export default function Footer() {
                 <Text className="text-[0.75rem] text-center text-neutral-text-highlight md:text-left md:pr-6 lg:pr-16 xl:pr-0 xl:w-3/4">
                     At MusioShore, we are dedicated to bringing music lovers closer to the sounds they cherish. Our mission is to provide high-quality audio products that enhance every listening experience, whether at home, on the go, or in the studio. We strive for excellence in design, durability, and performance, ensuring that every product delivers immersive sound.
                 </Text>
+                <Newsletter />
                 {/* Copyright and developer information */}
                 <Legal />
             </Container>
