83ffd0020626664f10a697745ee5169001a2cd9e
RamssCR <ramsesgpm@gmail.com>
2025-04-12 12:01:11 -0500

    style: rendered 3 cards aligned from tablet views on

--- src/components/new-products/NewProducts.tsx
+++ src/components/new-products/NewProducts.tsx
@@ -6,7 +6,7 @@ export default function NewProducts() {
     return (
         <section className="w-full flex flex-col items-center gap-12 py-5">
             <Title as="h2" className="text-3xl uppercase">Check our new products</Title>
-            <section className="w-full px-2 grid grid-cols-1 gap-y-5 md:grid-cols-2 md:gap-x-5 lg:grid-cols-3 xl:grid-cols-4">
+            <section className="w-full px-2 grid grid-cols-1 gap-y-5 md:grid-cols-3 md:gap-x-5 xl:grid-cols-4">
                 <ProductCard />
                 <ProductCard />
                 <ProductCard />
