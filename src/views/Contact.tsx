bec76bab21ecf0575313d270d55b21cbdca36533
RamssCR <ramsesgpm@gmail.com>
2025-04-18 12:23:12 -0500

    style: added more padding to the contact view container

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -3,7 +3,7 @@ import Container from "@components/ui/containers/Container"
 
 export default function Contact() {
     return (
-        <section className="mt-24 w-full px-6 py-6 bg-neutral-background md:px-8">
+        <section className="mt-24 w-full px-6 py-6 bg-neutral-background md:px-8 md:py-12">
             <Container className="mx-auto w-full flex flex-col items-center gap-8">
                 <ContactInfo />
             </Container>
